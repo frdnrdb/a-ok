c81bd0d535969f76641765ecac3370b955ce6c64
frd nrdb <froden@gmail.com>
2022-11-15 11:07:04 +0100

    💀

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 import assert from 'assert';
 
-const _ = {
-    log: console.log,
-    write: w => process.stdout.write(w),
-    tests: [],
-    ok: 0, err: 0, tot: 0, skip: 0,
-    mark: { ok: '✓', err: '✕', skip: '-', res: '=', async: '>', space: ' ', tab: '   →' }
-};
+const _ = { tests: [], ok: 0, err: 0, tot: 0, skip: 0 };
 
 const ansi = (() => {
     const map = { reset: 0, italic: 3, black: 30, red: 31, blue: 34, blue_: 44, green: 32, magenta: 35, cyan: 36, white: 37 };
@@ -14,38 +8,41 @@ const ansi = (() => {
     return (msg, [c1, c2]) => `${encode(map[c1] + (c2 ? `;${map[c2]}` : ''))}${msg}${encode(0)}`;
 })();
 
-const log = {
-    ok: (w, info) => {
-        ++_.tot && ++_.ok && _.log(_.mark.ok, ansi(w, ['green']), info ? ansi(info, ['blue', 'italic']) : '');
-    },
-    skip: (w, info) => {
-        ++_.tot && ++_.skip && _.log(_.mark.skip, ansi(w, ['white']), info ? ansi(info, ['blue', 'italic']) : '');
-    },
-    err: (w, { expected, actual }) => {
-        ++_.err;
-        _.log(_.mark.err, ansi(w, ['red']));
-        _.log(_.mark.tab, ansi(`exp ${expected}`, ['cyan', 'italic']));
-        _.log(_.mark.tab, ansi(`got ${actual}`, ['magenta', 'italic']));
-    },
-    res: () => {
-        _.err
-            ? _.log('\n' + _.mark.res, ansi(`${(100 - 100 * _.err/_.tot).toFixed(1)}% (${_.tot - _.err}/${_.tot})\n`, ['red']))
-            : _.log('\n' + _.mark.res, ansi(`100% (${_.ok}/${_.ok})${_.skip ? ansi(` – skipped ${_.skip}`, ['blue']) : ''}\n`, ['cyan']));
-    },
-    line: () => {
-        _.log(_.mark.space, ansi('-'.repeat(process.stdout.columns - 2), ['blue']));
-    },
-    label: w => {
-        _.log('\n' + _.mark.space, ansi(w, ['reset']));
-    },
-    group: w => {
-        _.log('\n' + _.mark.space, ansi(w, ['black', 'blue_']));
+const mark = { ok: '✓', err: '✕', skip: '-', res: '=', async: '>', space: ' ', tab: '   →' };
+const log = ((log = console.log) => {
+    return {
+        ok: (w, info) => {
+            ++_.tot && ++_.ok && log(mark.ok, ansi(w, ['green']), info ? ansi(info, ['blue', 'italic']) : '');
+        },
+        skip: (w, info) => {
+            ++_.tot && ++_.skip && log(mark.skip, ansi(w, ['white']), info ? ansi(info, ['blue', 'italic']) : '');
+        },
+        err: (w, { expected, actual }) => {
+            ++_.err;
+            log(mark.err, ansi(w, ['red']));
+            log(mark.tab, ansi(`exp ${expected}`, ['cyan', 'italic']));
+            log(mark.tab, ansi(`got ${actual}`, ['magenta', 'italic']));
+        },
+        res: () => {
+            _.err
+                ? log('\n' + mark.res, ansi(`${(100 - 100 * _.err/_.tot).toFixed(1)}% (${_.tot - _.err}/${_.tot})\n`, ['red']))
+                : log('\n' + mark.res, ansi(`100% (${_.ok}/${_.ok})${_.skip ? ansi(` – skipped ${_.skip}`, ['blue']) : ''}\n`, ['cyan']));
+        },
+        line: () => {
+            log(mark.space, ansi('-'.repeat(process.stdout.columns - 2), ['blue']));
+        },
+        label: w => {
+            log('\n' + mark.space, ansi(w, ['reset']));
+        },
+        group: w => {
+            log('\n' + mark.space, ansi(w, ['black', 'blue_']));
+        }
     }
-};
+})();
 
 function run (message, ...args) {
+    if (typeof this === 'function') return this();
     try {
-        if (typeof this === 'function') return this();
         assert[this || 'strictEqual'](...args.slice(0, 2), message);
         log[this === 'ok' ? 'skip' : 'ok'](message, args[2]);
     } catch(error) {
@@ -62,14 +59,13 @@ const prepare = async () => {
         const assertFunc = /reject|throw/i.test(args.scope || '');
 
         if (!assertFunc && (promise || func)) {
-            _.write(_.mark.async + ' ' + ansi(args[0], ['cyan', 'italic']));
+            process.stdout.write(mark.async + ' ' + ansi(args[0], ['cyan', 'italic']));
             args[1] = promise ? await args[1] : await args[1]();
-            _.write('\r');
+            process.stdout.write('\r');
         }
 
         run.call(args.scope, ...args);
     }
-
     log.res();
     process.exit(_.err ? 1 : 0);
 };
