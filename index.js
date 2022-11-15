f52f2d41259d2a61875f90903fe85017ee074304
frd nrdb <froden@gmail.com>
2022-11-15 10:55:44 +0100

    obfuscate 🙃

--- index.js
+++ index.js
@@ -1,122 +1,95 @@
 import assert from 'assert';
 
-const ansi = nr => `\u001b[${nr}m`;
-const applyAnsi = (txt, col = 'white', bg) => `${ansi(col + (bg ? `;${bg}` : ''))}${txt}${ansi(0)}`;
-
-const res = {
-    ok: 0,
-    err: 0,
-    skip: 0
+const _ = {
+    log: console.log,
+    write: w => process.stdout.write(w),
+    tests: [],
+    ok: 0, err: 0, tot: 0, skip: 0,
+    mark: { ok: '✓', err: '✕', skip: '-', res: '=', async: '>', space: ' ', tab: '   →' }
 };
 
-const log = (() => {
-    const symbols = {
-        ok: '✓',
-        err: '✕',
-        skip: '-',
-        res: '\n=',
-        info: '\n ',
-        space: ' '
-    };
-
-    function log(arg, ...args) {
-        const parts = args.reduce((arr, arg) => {
-            typeof arg === 'string' ? arr.push([arg]) : arr[arr.length-1].push(arg);
-            return arr
-        }, []);
-        console.log(arg, ...parts.map(arr => applyAnsi(...arr)));
-    }
-
-    return {
-        ok: (text, info) => {
-            log(symbols.ok, text, 32, ...(info ? [ info, 34, 3 ] : []));
-            res.ok++;
-        },
-        skip: (text, info) => {
-            log(symbols.skip, text, 37, ...(info ? [ info, 34, 3 ] : []));
-            res.skip++;
-        },
-        err: (text, { expected, actual }) => {
-            log(symbols.err, text, 31, `\n   → exp ${expected}`, 36, 3, `\n   → got ${actual}`, 35, 3);
-            res.err++;
-        },
-        res: () => {
-            if (res.err) {
-                const total = res.ok + res.err;
-                return log(symbols.res, `${(100 - 100 * res.err/total).toFixed(1)}% (${total - res.err}/${total})\n`, 31);
-            };
-            log(symbols.res, `100% (${res.ok}/${res.ok})${res.skip ? applyAnsi(` – skipped ${res.skip}`, 34) : ''}\n`, 36);
-        },
-        line: () => {
-            log(symbols.space, '-'.repeat(process.stdout.columns - 2), 34);
-        },
-        label: w => {
-            log(symbols.info, w, 0);
-        },
-        group: w => {
-            log(symbols.info, w, 30, 44);
-        }
-    };
+const ansi = (() => {
+    const map = { reset: 0, italic: 3, black: 30, red: 31, blue: 34, blue_: 44, green: 32, magenta: 35, cyan: 36, white: 37 };
+    const encode = code => `\u001b[${code}m`;
+    return (msg, [c1, c2]) => `${encode(map[c1] + (c2 ? `;${map[c2]}` : ''))}${msg}${encode(0)}`;
 })();
 
-function runTest (message, ...args) {
-    if (typeof this === 'function') return this();
+const log = {
+    ok: (w, info) => {
+        ++_.tot && ++_.ok && _.log(_.mark.ok, ansi(w, ['green']), info ? ansi(info, ['blue', 'italic']) : '');
+    },
+    skip: (w, info) => {
+        ++_.tot && ++_.skip && _.log(_.mark.skip, ansi(w, ['white']), info ? ansi(info, ['blue', 'italic']) : '');
+    },
+    err: (w, { expected, actual }) => {
+        ++_.err;
+        _.log(_.mark.err, ansi(w, ['red']));
+        _.log(_.mark.tab, ansi(`exp ${expected}`, ['cyan', 'italic']));
+        _.log(_.mark.tab, ansi(`got ${actual}`, ['magenta', 'italic']));
+    },
+    res: () => {
+        _.err
+            ? _.log('\n' + _.mark.res, ansi(`${(100 - 100 * _.err/_.tot).toFixed(1)}% (${_.tot - _.err}/${_.tot})\n`, ['red']))
+            : _.log('\n' + _.mark.res, ansi(`100% (${_.ok}/${_.ok})${_.skip ? ansi(` – skipped ${_.skip}`, ['blue']) : ''}\n`, ['cyan']));
+    },
+    line: () => {
+        _.log(_.mark.space, ansi('-'.repeat(process.stdout.columns - 2), ['blue']));
+    },
+    label: w => {
+        _.log('\n' + _.mark.space, ansi(w, ['reset']));
+    },
+    group: w => {
+        _.log('\n' + _.mark.space, ansi(w, ['black', 'blue_']));
+    }
+};
 
+function run (message, ...args) {
     try {
+        if (typeof this === 'function') return this();
         assert[this || 'strictEqual'](...args.slice(0, 2), message);
         log[this === 'ok' ? 'skip' : 'ok'](message, args[2]);
     } catch(error) {
-        const { code } = error;
-        if (code === 'ERR_ASSERTION') {
-            return log.err(message, error);
-        }
-        console.error(error);
+        error.code === 'ERR_ASSERTION' ? log.err(message, error) : console.error(error);
     }
 };
 
-const test = (timeout => {
-    console.clear();
-    
-    const tests = [];
+const prepare = async () => {
+    console.clear() || log.line();
 
-    return function() {
-        if (typeof this) arguments.scope = this; // assert[this] or log function this()
-        tests.push(arguments);
+    for (const args of _.tests) {
+        const promise = args[1] instanceof Promise;
+        const func = typeof args[1] === 'function';
+        const assertFunc = /reject|throw/i.test(args.scope || '');
 
-        timeout = clearTimeout(timeout) || setTimeout(async () => {
-            log.line();
-            for (const args of tests) {
-                const isPromise = args[1] instanceof Promise;
-                const isFunction = typeof args[1] === 'function';
-                const isAssertFunction = /reject|throw/i.test(args.scope || '');
+        if (!assertFunc && (promise || func)) {
+            _.write(_.mark.async + ' ' + ansi(args[0], ['cyan', 'italic']));
+            args[1] = promise ? await args[1] : await args[1]();
+            _.write('\r');
+        }
 
-                if (!isAssertFunction && (isPromise || isFunction)) {
-                    process.stdout.write('> ' + applyAnsi(args[0], 36, 3));
-                    args[1] = isPromise ? await args[1] : await args[1]();
-                    process.stdout.write('\r');
-                }
+        run.call(args.scope, ...args);
+    }
 
-                runTest.call(args.scope, ...args);
-            }
-            log.res();
-            process.exit(res.err ? 1 : 0);
-        }, 10);
-    };
-})();
+    log.res();
+    process.exit(_.err ? 1 : 0);
+};
 
-test.group = w => test.call(() => log.group(w));
-test.label = w => test.call(() => log.label(w));
+const test = function() {
+    if (typeof this) arguments.scope = this;
+    _.tests.push(arguments);
+    _.timeout = clearTimeout(_.timeout) || setTimeout(prepare, 10);
+};
 
 Object.entries(assert).forEach(([ key, value ]) => {
-    if (typeof value !== 'function') return;
-    if (/^AssertionError|ifError|strict|CallTracker$/.test(key)) return;
-    test[key] = function() {
+    typeof value === 'function' && (test[key] = function() {
         test.call(key, ...arguments);
-    };
+    });
 });
 
 test.skip = function() {
     test.call('ok', ...arguments);
 };
+
+['group', 'label'].forEach(prop => test[prop] = w => test.call(() => log[prop](w)));
 
 export default test;
